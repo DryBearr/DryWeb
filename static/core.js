b3e3ca929a5464d4ce106eab983c59b66501a29b
DryBearr <drybearr1@gmail.com>
2025-07-15 20:02:38 +0400

    added controls to page and changed styles

--- static/core.js
+++ static/core.js
@@ -7,8 +7,7 @@
 */
 
 //Config
-import config from "./available_wasms.json" with { type: "json" };
-let loadWasm = config.default_wasm_path;
+import config from "./config.json" with { type: "json" };
 
 //Insert links
 const nav = document.getElementById("wasm-links");
@@ -18,8 +17,10 @@ const currentHref = urlParams.get("wasm") || config.default_wasm;
 
 Object.entries(config.available_wasms).forEach(([key, path]) => {
   const link = document.createElement("a");
-  link.innerText = key;
+  const span = document.createElement("span");
+  span.textContent = key;
   link.href = `?wasm=${key}`;
+  link.appendChild(span);
   if (key === currentHref) {
     link.classList.add("active-link");
   }
@@ -28,6 +29,7 @@ Object.entries(config.available_wasms).forEach(([key, path]) => {
 
 //Try to load wasm base on given param in url
 const queryString = new URLSearchParams(window.location.search);
+let loadWasm = config.default_wasm_path;
 
 if (queryString.has("wasm")) {
   const wasmParam = queryString.get("wasm");
@@ -39,33 +41,36 @@ if (queryString.has("wasm")) {
 
 console.log("[Core] Using wasm:", loadWasm);
 
+//Create Window for renderer
+
+let { width, height } = config.window_size.default;
+if (isMobileViewport()) {
+  ({ width, height } = config.window_size.mobile);
+}
+
+const anchor = document.querySelector("main") || document.querySelector("body");
+if (!anchor) {
+  console.error("[Core] can't find main or body element wtf bro.");
+}
+
+const windowDiv = document.createElement("div");
+windowDiv.setAttribute("class", "render-window");
+anchor.append(windowDiv);
+
 // Init Canvas and off screen canvas
 const canvas = document.createElement("canvas");
 canvas.setAttribute("id", "renderer");
 
-const canvasParent =
-  document.querySelector("main") || document.querySelector("body");
-if (!canvasParent) {
-  console.error("[Core] no body or main element. bro wtf ...");
-}
-
-const width = canvasParent.offsetWidth;
-const height = canvasParent.offsetHeight;
-
-if (!width || !height) {
-  console.error("[Core] can't get height & width. bro wtf ...");
-}
-
 canvas.setAttribute("width", width);
 canvas.setAttribute("height", height);
-canvasParent.append(canvas);
+windowDiv.append(canvas);
 
 const offScreenCanvas = canvas.transferControlToOffscreen();
 
 // Init worker for wasm
 console.log("[Core] Init workers...");
 
-const workerApi = new Worker("worker_api.js", { type: "module" });
+let workerApi = new Worker("worker_api.js", { type: "module" });
 const workerCanvas = new Worker("worker_canvas.js", { type: "module" });
 
 workerApi.postMessage({
@@ -101,25 +106,114 @@ workerApi.addEventListener("message", function (event) {
   }
 });
 
-const resizeObserver = new ResizeObserver((entries) => {
-  for (let entry of entries) {
-    const width = entry.contentRect.width;
-    const height = entry.contentRect.height;
+//Resize options
 
-    console.log("[Core] Resized to", width, height);
+const controlsDiv = document.createElement("div");
+controlsDiv.setAttribute("class", "controls");
+let widthResizeOption = { max: window.innerWidth, min: 0 };
+let heightResizeOption = { max: window.innerHeight, min: 0 };
 
+if (!isMobileViewport()) {
+  const widthInput = document.createElement("input");
+  widthInput.type = "number";
+  widthInput.placeholder = "Width";
+  widthInput.value = config.window_size.default.width;
+
+  const heightInput = document.createElement("input");
+  heightInput.type = "number";
+  heightInput.placeholder = "Height";
+  heightInput.value = config.window_size.default.height;
+
+  const applyButton = document.createElement("button");
+  applyButton.textContent = "Apply Size";
+  applyButton.addEventListener("click", () => {
+    const tempWidth = parseInt(widthInput.value, 10);
+    const tempHeight = parseInt(heightInput.value, 10);
+
+    if (
+      !isNaN(tempWidth) &&
+      !isNaN(tempHeight) &&
+      tempWidth > widthResizeOption.min &&
+      tempWidth < widthResizeOption.max &&
+      tempHeight > heightResizeOption.min &&
+      tempHeight < heightResizeOption.max
+    ) {
+      width = tempWidth;
+      height = tempHeight;
+
+      workerApi.postMessage({
+        type: "resize",
+        width: width,
+        height: height,
+      });
+    } else {
+      ({ width, height } = config.window_size.default);
+      workerApi.postMessage({
+        type: "resize",
+        width: width,
+        height: height,
+      });
+
+      heightInput.value = height;
+      widthInput.value = width;
+    }
+  });
+
+  const defaultSizeButton = document.createElement("button");
+  defaultSizeButton.textContent = "default size";
+  defaultSizeButton.addEventListener("click", () => {
+    ({ width, height } = config.window_size.default);
     workerApi.postMessage({
       type: "resize",
-      width: Math.floor(width),
-      height: Math.floor(height),
+      width: width,
+      height: height,
     });
-  }
+    heightInput.value = height;
+    widthInput.value = width;
+  });
+
+  controlsDiv.append(widthInput, heightInput, applyButton, defaultSizeButton);
+}
+
+anchor.append(controlsDiv);
+
+//Reload Wasm
+const reloadWasmButton = document.createElement("button");
+reloadWasmButton.textContent = "Reload";
+reloadWasmButton.addEventListener("click", () => {
+  workerApi.terminate();
+
+  workerApi = new Worker("./worker_api.js", { type: "module" });
+
+  workerApi.postMessage({
+    type: "init",
+    wasm: loadWasm,
+    width: width,
+    height: height,
+  });
+
+  workerApi.addEventListener("message", function (event) {
+    const data = event.data;
+    if (data.type === "frame") {
+      workerCanvas.postMessage({
+        ...data,
+      });
+    }
+  });
+
+  workerApi.addEventListener("message", function (event) {
+    const data = event.data;
+    if (data.type === "framePart") {
+      workerCanvas.postMessage({
+        ...data,
+      });
+    }
+  });
 });
+reloadWasmButton.setAttribute("class", "reload-button");
 
-resizeObserver.observe(canvasParent);
-
-//Drag event logic
-
+controlsDiv.append(reloadWasmButton);
+//On Canvas Drag event logic
 const getCanvasCoordinates = (event) => {
   const rect = canvas.getBoundingClientRect();
   if (event.touches && event.touches.length > 0) {
@@ -203,3 +297,8 @@ canvas.addEventListener("touchmove", (event) => handleDragMove(event));
 canvas.addEventListener("touchend", (event) => handleDragEnd(event));
 
 console.log("[Core] Init workers Done.");
+
+//Utility functions
+function isMobileViewport() {
+  return window.matchMedia("(max-width: 767px)").matches;
+}
